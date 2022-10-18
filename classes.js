ef46aa55a281de07f7adcd1a92c28f49174bae47
lucas <lucas.gatica79@gmail.com>
2022-10-18 14:20:19 -0300

    exercicio classes

---
+++ classes.js
@@ -0,0 +1,55 @@
+// Task 1: Code a Person class
+
+class Person{
+    constructor(name ="Tom",age=20,energy =100){
+        this.name = name
+        this.age = age
+        this.energy = energy
+    }
+    
+
+    sleep() {
+        this.energy +=10
+    }
+
+    doSomethingFun() {
+        this.energy -=10
+        
+    }
+
+}
+
+
+
+// Task 2: Code a Worker class
+class Worker extends Person{
+    constructor( name,age,energy,xp=0, hourlyWage=10) {
+        super(name, age, energy)
+        this.xp=xp
+        this.hourlyWage=hourlyWage
+    }
+    goToWork() {
+        this.xp +=10
+    }
+}
+
+
+// Task 3: Code an intern object, run methods
+function intern(name,age,energy,xp,hourlyWage) {
+    let intern = new Worker(name,age,energy,xp,hourlyWage)
+    intern.goToWork()
+    return intern
+}
+let intern1 = intern("Bob", 21, 110, 0, 10)
+
+
+
+// Task 4: Code a manager object, methods
+
+function manager(name, age, energy, xp, hourlyWage) {
+    let manager = new Worker(name,age,energy,xp,hourlyWage)
+    manager.doSomethingFun()
+    return manager
+}
+
+let manager1 = manager("Alice", 30, 120, 100, 30)
